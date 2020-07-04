28b5fdfa1461ecdcecb5f651d4465ad58165797a
jdomingr <juandominguezriquelme@gmail.com>
2020-07-04 14:24:54 -0400

    Cambios menores

---
+++ docs/precache-manifest.5ce72872435d4a89853267bbf5a00181.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08bbcb1530ab8a3cf59542471b70a21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fed22045dc5bee94938",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "eedbc748afaaed15e8fa",
+    "url": "/static/css/main.5a454c55.chunk.css"
+  },
+  {
+    "revision": "4fed22045dc5bee94938",
+    "url": "/static/js/2.7f4a3fdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f4a3fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eedbc748afaaed15e8fa",
+    "url": "/static/js/main.e2b26299.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
